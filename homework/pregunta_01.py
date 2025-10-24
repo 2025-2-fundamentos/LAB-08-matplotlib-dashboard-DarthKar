43298ef96156b7bbf6416358ed52b08e7fb57a96
Miguel Angel Peña Marin <mipenama@unal.edu.co>
2025-10-23 23:26:56 -0500

    sol

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -35,37 +35,44 @@ def pregunta_01():
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
-    import pandas as pd
-    import matplotlib.pyplot as plt
     import os
+    import pandas as pd
+    import matplotlib
+    matplotlib.use("Agg")  
+    import matplotlib.pyplot as plt
 
-    os.makedirs('files\docs', exist_ok=True)
-    
-    df = pd.read_csv('files\input\shipping-data.csv')
+
+    os.makedirs(os.path.join('docs'), exist_ok=True)
+
+
+    df = pd.read_csv(os.path.join('files', 'input', 'shipping-data.csv'))
+
+    # --- Shippings per Warehouse ---
     plt.figure()
     counts = df.Warehouse_block.value_counts()
     counts.plot.bar(
         title='Shippings per Warehouse',
-        xlabel = 'Warehouse block',
-        ylabel = 'Record Count',
-        color = 'tab:blue',
-        fontsize = 8
+        xlabel='Warehouse block',
+        ylabel='Record Count',
+        color='tab:blue',
+        fontsize=8
     )
     plt.gca().spines['top'].set_visible(False)
     plt.gca().spines['right'].set_visible(False)
+    plt.savefig(os.path.join('docs', 'shipping_per_warehouse.png'))
 
-    plt.savefig('files\docs\shipping_per_warehouse.png')
-
+    # --- Mode of shipment ---
     plt.figure()
     counts = df.Mode_of_Shipment.value_counts()
     counts.plot.pie(
-        title = 'Mode of shipment',
-        wedgeprops = dict(width = 0.35),
-        ylabel = "",
-        colors = ["tab:blue", "tab:orange", "tab:green"]
+        title='Mode of shipment',
+        wedgeprops=dict(width=0.35),
+        ylabel="",
+        colors=["tab:blue", "tab:orange", "tab:green"]
     )
-    plt.savefig('files\docs\mode_of_shipment.png')
+    plt.savefig(os.path.join('docs', 'mode_of_shipment.png'))
 
+    # --- Average Customer Rating ---
     plt.figure()
     rate = (
         df[['Mode_of_Shipment', 'Customer_rating']]
@@ -73,46 +80,44 @@ def pregunta_01():
         .describe()
     )
     rate.columns = rate.columns.droplevel()
-    rate = rate[["mean",'min','max']]
+    rate = rate[["mean", 'min', 'max']]
+
     plt.barh(
-        y = rate.index.values,
-        width = rate['max'].values - 1,
-        left = rate['min'].values,
+        y=rate.index.values,
+        width=rate['max'].values - 1,
+        left=rate['min'].values,
         height=0.9,
-        color= 'lightgray',
-        alpha = 0.8
+        color='lightgray',
+        alpha=0.8
     )
     colors = [
         "tab:green" if value >= 3.0 else "tab:orange" for value in rate["mean"].values
     ]
     plt.barh(
-        y = rate.index.values,
-        width = rate['mean'].values - 1,
-        left = rate['min'].values,
+        y=rate.index.values,
+        width=rate['mean'].values - 1,
+        left=rate['min'].values,
         height=0.5,
-        color= colors,
-        alpha = 1.0
+        color=colors,
+        alpha=1.0
     )
     plt.title('Average Customer Rating')
     plt.gca().spines['top'].set_visible(False)
     plt.gca().spines['right'].set_visible(False)
     plt.gca().spines['bottom'].set_color('grey')
     plt.gca().spines['left'].set_color('grey')
+    plt.savefig(os.path.join('docs', 'average_customer_rating.png'))
 
-    plt.savefig('files/docs/average_customer_rating.png')
-
-
+    # --- Shipped Weight Distribution ---
     plt.figure()
     df.Weight_in_gms.hist(
-        color = 'lightgreen',
-        edgecolor = 'black',
-        grid = False
+        color='lightgreen',
+        edgecolor='black',
+        grid=False
     )
     plt.title('Shipped Weight Distribution')
     plt.gca().spines['top'].set_visible(False)
     plt.gca().spines['right'].set_visible(False)
-    
-    plt.savefig('files\docs\shipped_weight_distribution.png')
+    plt.savefig(os.path.join('docs', 'weight_distribution.png'))
 
-    
 pregunta_01()
